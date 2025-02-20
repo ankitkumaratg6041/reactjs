201cd401bf053cb7dfbd821d254e7638d8cfbfd2
ankitkumaratg6041 <ankitkumaratg6041@gmail.com>
2025-02-19 19:51:06 -0700

    secret keys deleted

--- Episode-6/utils/constants.js
+++ Episode-6/utils/constants.js
@@ -1,6 +1,3 @@
 export const CDN_URL = 'https://res.cloudinary.com/rainforest-cruises/images/c_fill,g_auto/f_auto,q_auto/w_1120,h_732,c_fill,g_auto/v1661347444/india-food-butter-chicken/india-food-butter-chicken-1120x732.jpg';
 export const LOGO_URL = 'https://img.pikbest.com/png-images/20241030/culinary-restaurant-logo-design_11027332.png!sw800';
-export const ACCESS_KEY = 'LonS535z15B1rGJH2rPVWKBPlKUP-hIzFVaw5V7Qz4Q';
-export const SECRET_KEY = 'i04kwPVzu6ZEWCvxJ_5ob7kRAZkFJ0CdqZl2yo6MhdM';
-const applicationId = '711600';
 export const SWIGGY_IMG_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/';
